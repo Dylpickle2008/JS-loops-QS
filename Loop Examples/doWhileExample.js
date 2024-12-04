6ea78fb72fb3534300d3d1b3571d7f3aa142b9b1
Dylpickle2008 <dylanrigby1016@gmail.com>
2024-12-04 13:34:16 +0000

    added to doWhile and While

--- Loop Examples/doWhileExample.js
+++ Loop Examples/doWhileExample.js
@@ -1,1 +1,26 @@
+let outputDiv = document.getElementById("output");
+let passwordInput = document.getElementById("passwordInput");
+let submitButton = document.getElementById("submitButton");
+
+const correctPassword = "12345";
+outputDiv.innerHTML += "<h2>Using a password prompt</h2>";
+
+submitButton.addEventListener("click", () =>{
+    let password; 
+    let attempt = 0;
+
+    do{
+        password = passwordInput.value;
+
+        if(password === correctPassword){
+            outputDiv.innerHTML += "Access Granted!<br>";
+            passwordInput.disabled = true;
+            submitButton.disabled = true; 
+            break;
+        }else{
+            outputDiv.innerHTML += "Incorrect password. Attempt ${++attempt}. Try again.<br>";
+            passwordInput.value = ";"
+        }
+    } while (password !== correctPassword && !passwordInput.disabled);
+});
 
